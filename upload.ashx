ffd15012236e8682c1b38ac21530b0fd690e6e88
Willard <913483237@qq.com>
2021-01-11 18:16:02 +0800

    a

---
+++ upload.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="upload.ashx.cs" Class="h5.audio.upload" %>
---
+++ upload.ashx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace h5.audio
+{
+    /// <summary>
+    /// upload 的摘要说明
+    /// </summary>
+    public class upload : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var mp3name = context.Request.Params["mp3Name"];
+            var dir = "\\audio\\upload\\";
+            var path = dir + mp3name;
+            var localPath = context.Server.MapPath("~/") + path;
+            context.Request.Files[0].SaveAs(localPath);
+            context.Response.Write(path);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
